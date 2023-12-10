d83ca2cf506f80a82b73e0d0237af94f782375aa
ngdnhtien <ngdnhtien_t65@hus.edu.vn>
2023-12-10 12:11:05 +0700

    qubit 109

--- pulse.py
+++ pulse.py
@@ -1,13 +1,26 @@
-# Last updated: 6th December 2023
+# Last updated: 10th December 2023
 
-# Measurement pulse
+# QUBIT 0
+
+## Measurement pulse
 
 meas_amplitude = 0.6
 meas_freq_shifted = -1*MHz
 
-# Control pulses
+## Control pulses
 
 pi_amp_12 = 0.24029996372238208
 pi_dur_12 = 160
 pi_sigma_12 = 160/4
 pi_beta_12 = 2.0
+
+# QUBIT 109
+
+## Measurement pulse: N/A
+
+## Control pulses
+
+pi_amp_12 = 0.2234824425369455
+pi_dur_12 = 96
+pi_sigma_12 = 24
+pi_beta_12 = -0.5
